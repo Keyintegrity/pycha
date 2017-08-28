b4f87f717b1b23b9966f7b4a6e355cb564e47be5
Artyom Opanchuk <artyom.opanchuk@gmail.com>
2017-08-28 16:04:05 +0300

    Add classifiers

--- setup.py
+++ setup.py
@@ -51,4 +51,13 @@ setup(
         ]
     },
     test_suite="tests",
+    classifiers=[
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
 )
